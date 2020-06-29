dd4b7c6e3049c5ea3daca3cc0f554f7e4f1fd542
zanussbaum <zanussbaum@gmail.com>
2020-06-29 14:45:15 -0400

    WIP tensorboard

--- pix2pix.py
+++ pix2pix.py
@@ -22,7 +22,7 @@ class Pix2Pix:
             self.log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         )
         self.writer2 = tf.summary.create_file_writer(
-            self.log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            self.log_dir + "val/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         )
         
 
@@ -204,9 +204,9 @@ class Pix2Pix:
             tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=epoch)
             tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=epoch)
             tf.summary.scalar('disc_loss', disc_loss, step=epoch)
-            tf.summary.scalar('loss', real_loss, step=epoch, description="Real Loss")
+            tf.summary.scalar('loss', real_loss, step=epoch)
         with self.writer2.as_default():
-            tf.summary.scalar('loss', fake_loss, step=epoch, description="Fake Loss")
+            tf.summary.scalar('loss', fake_loss, step=epoch)
 
     
 
