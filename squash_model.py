4a40a70edf2e7af1d71c9baa0fa6542bf794618b
zanussbaum <zanussbaum@gmail.com>
2020-07-01 13:48:28 -0400

    making weights non trainable after

--- squash_model.py
+++ squash_model.py
@@ -2,20 +2,20 @@ import tensorflow as tf
 import pathlib
 
 def squash_and_save_model(model):
-    discriminator = model.discriminator
     generator = model.generator
+    generator.trainable = False
+    for layer in generator.layers:
+        layer.trainable = False
     
-    d_converter, g_converter = tf.lite.TFLiteConverter.from_keras_model(discriminator), tf.lite.TFLiteConverter.from_keras_model(generator)
-    d_converter.optimizations, g_converter.optimizations = [tf.lite.Optimize.DEFAULT], [tf.lite.Optimize.DEFAULT]
+    g_converter = tf.lite.TFLiteConverter.from_keras_model(generator)
+    g_converter.optimizations = [tf.lite.Optimize.DEFAULT]
     
-    d_tflite_quant_model, g_tflite_quant_model  = d_converter.convert(), g_converter.convert()
+    g_tflite_quant_model  = g_converter.convert()
 
     tflite_models_dir = pathlib.Path("mocap_models/")
     tflite_models_dir.mkdir(exist_ok=True, parents=True)
     tflite_gen_model_file = tflite_models_dir/"generator.tflite"
-    tflite_discr_model_file = tflite_models_dir/"discriminator.tflite"
 
-    tflite_discr_model_file.write_bytes(d_tflite_quant_model)
     tflite_gen_model_file.write_bytes(g_tflite_quant_model)
 
-    return d_tflite_quant_model, g_tflite_quant_model
+    return g_tflite_quant_model
